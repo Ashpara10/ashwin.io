405cb6e25de5c57a1808f40a80e694a9872d0fc1
AshPara10 <ashwinparande1156610c@gmail.com>
2023-12-06 00:23:43 +0530

    ui change

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -27,7 +27,7 @@ const BlogCard = ({ data, index }: { data: Blog; index: number }) => {
       //   once: true,
       // }}
       // initial="initial"
-      className="w-full odd:-rotate-1 even:rotate-1 transition-transform hover:rotate-0 md:min-h-[200px] flex flex-col  gap-3 rounded-3xl border border-gray-300 bg-gray-100 dark:bg-dark dark:border-border "
+      className="w-full odd:-rotate-1 even:rotate-1 transition-transform hover:rotate-0 md:min-h-[200px] flex flex-col  gap-3 rounded-3xl border-2 border-gray-300 bg-gray-100 dark:bg-dark dark:border-border "
     >
       <div className="flex flex-col items-center gap-3 justify-center  gap-y-2 px-4 py-5">
         <h2 className="w-full text-left  font-semibold tracking-tight text-2xl  ">
