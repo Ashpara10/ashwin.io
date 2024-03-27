0e52c19f04396709fa0af4d67e08349ec45f775a
AshPara10 <ashwinparande1156610c@gmail.com>
2024-03-27 16:43:37 +0530

    ui change3

--- components/header.tsx
+++ components/header.tsx
@@ -63,18 +63,18 @@ const Header = () => {
   return (
     <header className="fixed top-0 w-full flex items-center justify-center z-20 backdrop-blur-md">
       <nav className=" mx-2 px-2 overflow-hidden   py-2.5  max-w-2xl  w-full flex items-center justify-end">
-        <div className="flex items-center justify-center gap-x-2">
+        <div className="flex z-30 items-center justify-center gap-x-2">
           {hasMounted && theme === "dark" ? (
             <button
               onClick={() => setTheme("light")}
-              className="p-2 rounded-xl focus-visible:outline-none text-sm bg-orange-200 text-black"
+              className="p-2  rounded-xl focus-visible:outline-none text-sm bg-orange-200 text-black"
             >
               <Sun className="text-sm" />
             </button>
           ) : (
             <button
               onClick={() => setTheme("dark")}
-              className="p-2 rounded-xl focus-visible:outline-none text-sm bg-purple-700 text-white"
+              className="p-2 rounded-xl  focus-visible:outline-none text-sm bg-purple-700 text-white"
             >
               {" "}
               <Moon className="text-sm" />
@@ -82,51 +82,57 @@ const Header = () => {
           )}
           <motion.button
             onClick={() => setShow(!show)}
-            className="focus-within:outline-none border dark:border-border dark:bg-dark bg-light p-2 rounded-xl"
+            className=" focus-within:outline-none border dark:border-border dark:bg-dark bg-light p-2 rounded-xl"
           >
             <Menu />
           </motion.button>
-          {show && (
-            <motion.div
-              className=" z-30 dark:bg-dark bg-white  rounded-3xl w-52 py-3  border border-gray-200/70 dark:border-border fixed top-16 "
-              initial={{ opacity: 0, translateY: -450, translateX: -60 }}
-              animate={{ translateY: 0, opacity: 1, translateX: -60 }}
-              exit={{ opacity: 0, translateY: -450 }}
-              transition={{ type: "tween" }}
-            >
-              {Object.entries(navItems).map(([path, { name }]) => {
-                return (
-                  <motion.div
-                    className="text-md px-3 flex items-center justify-start gap-x-0.5 w-full first:border-none border-t border-gray-200/70 dark:border-border py-2 focus:dark:bg-border"
-                    key={path}
-                    onClick={() => router.push(path)}
-                  >
-                    {/* <Dot
+        </div>
+        {show && (
+          <motion.div
+            onClick={() => setShow(false)}
+            className="w-full h-screen fixed z-10 bg-black/50 top-0 bottom-0 right-0 left-0"
+          />
+        )}
+        {show && (
+          <motion.div
+            className=" z-30 dark:bg-dark bg-white  rounded-3xl w-52 py-3  border border-gray-200/70 dark:border-border fixed top-16 "
+            initial={{ opacity: 0, translateY: -450 }}
+            animate={{ translateY: 0, opacity: 1 }}
+            exit={{ opacity: 0, translateY: -450 }}
+            transition={{ type: "tween" }}
+          >
+            {Object.entries(navItems).map(([path, { name }]) => {
+              return (
+                <motion.div
+                  className="text-md px-3 flex items-center justify-start gap-x-0.5 w-full first:border-none border-t border-gray-200/70 dark:border-border py-2 focus:dark:bg-border"
+                  key={path}
+                  onClick={() => router.push(path)}
+                >
+                  {/* <Dot
                       className="w-6 h-6"
                       style={{
                         visibility: pathname === path ? "visible" : "hidden",
                       }}
                     />{" "} */}
-                    {name}
-                  </motion.div>
-                );
-              })}
+                  {name}
+                </motion.div>
+              );
+            })}
 
-              {Object.entries(links).map(([name, { icon, link }]) => {
-                return (
-                  <motion.div
-                    key={name}
-                    className="text-md px-3 first:border-none border-t flex items-center justify-start gap-x-0.5 border-gray-200/70 dark:border-border py-1.5 focus:dark:bg-border"
-                  >
-                    <Link href={link} target="_blank">
-                      {name}
-                    </Link>
-                  </motion.div>
-                );
-              })}
-            </motion.div>
-          )}
-        </div>
+            {Object.entries(links).map(([name, { icon, link }]) => {
+              return (
+                <motion.div
+                  key={name}
+                  className="text-md px-3 first:border-none border-t flex items-center justify-start gap-x-0.5 border-gray-200/70 dark:border-border py-1.5 focus:dark:bg-border"
+                >
+                  <Link href={link} target="_blank">
+                    {name}
+                  </Link>
+                </motion.div>
+              );
+            })}
+          </motion.div>
+        )}
       </nav>
     </header>
   );
